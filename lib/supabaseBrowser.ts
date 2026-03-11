aa4243a078350707295573a08b3cca417b3b0ffb
eidenc134-create <eidenc134@gmail.com>
2026-03-11 15:50:08 +0000

    fix supabase browser client

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function supabaseBrowser() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
       if (!supabaseUrl) {
           throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL");
